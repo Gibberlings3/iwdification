3b8826551eaf7053b86b0653f0b1a258e16df38a
CamDawg <pcamagna@yahoo.com>
2021-07-25 12:55:44 -0700

    Fixing install errors for BGEE

--- package_mod.bat
+++ package_mod.bat
@@ -5,7 +5,7 @@ REM /* this initializes the values for the different variables referenced in com
 call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
-set "mod_name=Iwdification"
+set "mod_name=IWDification"
 set mod_version=RC1
 set mod_folder=iwdification
 
