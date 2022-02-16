fd5b054e615f6df5c8f5579cf2f683a646b257b1
CamDawg <pcamagna@yahoo.com>
2022-02-16 07:31:29 -0800

    (Great) Shout lacks a deaf icon
    Incorporating upstream fix in IWDSpells library:
    https://github.com/Gibberlings3/IWDSpells/commit/94039b1b441469b56d90290b1148de52dbd86b7b

--- package_mod.bat
+++ package_mod.bat
@@ -6,7 +6,7 @@ call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
 set "mod_name=IWDification"
-set mod_version=RC2
+set mod_version=v1
 set mod_folder=iwdification
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
